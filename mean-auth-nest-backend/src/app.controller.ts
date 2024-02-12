fe3f5ea0908296c186286e923ce37a723154c35f
Abel Borit <abelboritguitton@gmail.com>
2024-02-12 11:25:55 -0500

    [CHORE] first steps Nest

--- mean-auth-nest-backend/src/app.controller.ts
+++ mean-auth-nest-backend/src/app.controller.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  /* aquí tenemos la petición @Get() que tiene un método getHello() que se conecta a un servicio mediante una inyección de dependencias. Este servicio es el que está en el archivo app.service.ts */
   @Get()
   getHello(): string {
     return this.appService.getHello();
