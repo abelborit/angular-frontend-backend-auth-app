56b80fdd31f8ed1ac75e5a426385fe742206baab
Abel Borit <abelboritguitton@gmail.com>
2024-02-14 16:46:24 -0500

    [FEAT] auth-layout.component using "ng g c auth/layouts/authLayout --skip-selector"

--- mean-auth-angular-frontend/src/app/auth/auth.module.ts
+++ mean-auth-angular-frontend/src/app/auth/auth.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 
 @NgModule({
   declarations: [
     LoginPageComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
+    AuthLayoutComponent
   ],
   imports: [
     CommonModule,
