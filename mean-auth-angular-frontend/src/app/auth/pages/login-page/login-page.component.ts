d592547c5ae65828dac795cc06b0333d70166af0
Abel Borit <abelboritguitton@gmail.com>
2024-02-15 11:22:14 -0500

    [FEAT] reactive forms in login-page.component & html auth-layout.component

--- mean-auth-angular-frontend/src/app/auth/pages/login-page/login-page.component.ts
+++ mean-auth-angular-frontend/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent {
+  /* una recomendación en la inyección de dependencias es trabajar con la sintaxis de inject() ya que nos permite inyectar servicios en funciones lo cual es útil para que las aplicaciones en Angular sean más rápidas */
+  private formBuilder = inject(FormBuilder);
 
+  public myForm: FormGroup = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  login() {
+    console.log(this.myForm.value);
+  }
 }
