95c8ef7b13d80905370ea6c67f00fb5ba20e43f9
Abel Borit <abelboritguitton@gmail.com>
2024-02-15 14:30:28 -0500

    [FEAT] enable CORS (backend)

--- mean-auth-nest-backend/src/main.ts
+++ mean-auth-nest-backend/src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  /* habilitar el CORS */
+  app.enableCors();
+
   /* esta configuración dice: vamos a hacer muy restringido nuestro backend, es decir, tienen que mandarme la información correcta como se configuró y como se espera y si no entonces no se acepta y se rechaza */
   app.useGlobalPipes(
     new ValidationPipe({
