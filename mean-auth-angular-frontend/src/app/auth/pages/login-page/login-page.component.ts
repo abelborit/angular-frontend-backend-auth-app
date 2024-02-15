b3092284106740119f4f12e51091fe19e1eaa9f4
Abel Borit <abelboritguitton@gmail.com>
2024-02-15 17:39:33 -0500

    [FEAT] navigate to dashboard page & show user in dashboard page

--- mean-auth-angular-frontend/src/app/auth/pages/login-page/login-page.component.ts
+++ mean-auth-angular-frontend/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 import { AuthService } from '../../services/auth.service';
 
@@ -11,10 +12,11 @@ export class LoginPageComponent {
   /* una recomendación en la inyección de dependencias es trabajar con la sintaxis de inject() ya que nos permite inyectar servicios en funciones lo cual es útil para que las aplicaciones en Angular sean más rápidas */
   private formBuilder = inject(FormBuilder);
   private authService = inject(AuthService);
+  private router = inject(Router);
 
   public myForm: FormGroup = this.formBuilder.nonNullable.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+    email: ['correo1234@correo.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
 
   handleLogin() {
@@ -25,13 +27,15 @@ export class LoginPageComponent {
     this.authService.handleLogin(email, password).subscribe({
       next: (loginResponse) => {
         // console.log({ loginResponse });
+        /* al final se coloca el then() para que cuando acabe el timer del sweetalert2 me pueda redirigir a la página de dashboard */
         Swal.fire({
           icon: 'success',
           title: 'Correct Credentials',
           text: 'Redirecting to home page...',
-          timer: 2000,
+          timer: 1500,
           timerProgressBar: true,
-        });
+          showConfirmButton: false,
+        }).then(() => this.router.navigateByUrl('/dashboard'));
       },
       error: (loginError) => {
         // console.log({ loginError });
--- mean-auth-angular-frontend/src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
+++ mean-auth-angular-frontend/src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   templateUrl: './dashboard-layout.component.html',
-  styleUrls: ['./dashboard-layout.component.css']
+  styleUrls: ['./dashboard-layout.component.css'],
 })
 export class DashboardLayoutComponent {
+  private authService = inject(AuthService);
 
+  /* FORMA 1: obtener valor usando señales computadas */
+  public currentUser = computed(() => this.authService.currentUser());
+
+  /* FORMA 2: obtener valor usando getter */
+  // get currentUser() {
+  //   return this.authService.currentUser();
+  // }
 }
