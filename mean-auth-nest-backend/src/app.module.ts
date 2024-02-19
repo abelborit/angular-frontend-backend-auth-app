ad9878cd91a07c601e40108d424ac7eb5e943370
Abel Borit <abelboritguitton@gmail.com>
2024-02-19 09:47:21 -0500

    [FEAT] backend configurations

--- mean-auth-nest-backend/src/app.module.ts
+++ mean-auth-nest-backend/src/app.module.ts
@@ -10,7 +10,9 @@ import { AppService } from './app.service';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      dbName: process.env.MONGO_DB_NAME,
+    }),
     AuthModule,
   ],
   controllers: [AppController],
